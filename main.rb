f8d190281414d98cec1c0f4c22883a68899e1c20
fumi238000 <s21231128@gmail.com>
2020-12-02 13:24:51 +0900

    コメントの作成

--- main.rb
+++ main.rb
@@ -1,1 +1,36 @@
-pust "おはよう"
+# ------------------
+#1タスクを要求する
+
+
+
+#2タイトルを要求する
+
+
+
+#③表示する
+
+
+
+
+# Task-------------------------------
+
+# class Task
+
+# end
+
+
+
+
+# # ToDo-------------------------------
+# class ToDo
+#   def add
+#     puts "#{title:}"
+#   end
+
+# end
+
+
+# task1= Task.new(title: "洗濯", content:"７時半までに干し終える")
+# task2= Task.new(title: "仕事", content:"9~18時")
+# task3= Task.new(title: "買い物", content:"卵、ヨーグルト")
+
