9fb66c4303fa901b2b4ed4a68a09a6a3ff4ad69f
fumi238000 <s21231128@gmail.com>
2020-12-04 16:04:26 +0900

    概要の記入・gemfileの導入

--- main.rb
+++ main.rb
@@ -1,15 +1,146 @@
+
+
+##----------------------------------------Task
+#タスクは3つの情報を持つ
+#id, title, content
+# タスクのidは一から順番に振られる
+
+class Task
+  def initialize(**params)
+    title = :title
+    content = :content
+  end
+
+
+  def info
+    puts "受け取った値は#{title}です"
+  end
+
+end
+
+
+##----------------------------------------TODO
+# タスクの追加・タスクの一覧表示・削除の機能を持つ
+# タスクのIDを指定して削除を行う
+
+class ToDo
+
+  def def initialize
+    @tasks = []
+    puts "#{@task}"
+  end
+
+def add(task) 
+  @task = task
+  puts @task
+  p @task
+end
+
+
+# def info(task)
+#   puts "#{task}"
+# end
+
+
+
+end
+
+
+##-----------------------------------------以下main.rbの処理
+
+
+# 初期設定の導入
+
+task1 = Task.new(title: "洗濯", content: "8時までに干し終える")
+task2 = Task.new(title: "水を飲む", content: "500ml")
+
+todo = ToDo.new
+todo.add(task1)
+
+# todo.info
+# todo.info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ------------------
-#「タイトル」を要求する
-print "タイトルを入力 >"
-input_title = gets
+# # #「タイトル」を要求する
+# print "タイトルを入力 >"
+# input_title = gets.chomp
 
-#「タイトルのタスク」を要求する
-print "タスクを入力 >"
-input_task = gets.chomp
+# #「タイトルのタスク」を要求する
+# print "タスクを入力 >"
+# input_content = gets.chomp
 
-#③表示する
-puts "#{input_task}: #{input_title}"
+# #③表示する
+# puts "タイトル：#{input_title} ： タスク#{input_content}" 
 
+
+
+
+
+
+
+# task1 = Task.new(title: "洗濯", content: "7時半までに干し終える")
+
+
+# todo.add(task1)
+# todo.info 
+
+
+
+
+
+
+
+
+# 値を配列に格納する処理
+
+# p input_content
+
+# task = Task.new(input_title,input_content)
+
+# task.info
+
+
+
+
+
+
+
+
+
+# todo_list = []
+# todo_list << input_task
+
+# p todo_list
+# puts todo_list
+
+
+
+
+# タスクとタイトルを変数に格納する
+
+
+
+# 格納した配列を表示する処理(+ with_indexを追加する)
 
 
 
